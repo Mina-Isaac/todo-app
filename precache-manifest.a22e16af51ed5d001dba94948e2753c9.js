bbc4042834d97e1d76d754fa047c0394c0b67a22
Mina UAE <mina.isaac.2000@gmail.com>
2020-04-01 00:36:45 +0200

    Updates

---
+++ precache-manifest.a22e16af51ed5d001dba94948e2753c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9162a8c27d14b7ff6025ba402d914a4e",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "495715a57c09400b39d1",
+    "url": "/todo-app/static/css/main.37e8aa83.chunk.css"
+  },
+  {
+    "revision": "52f2188041e188929886",
+    "url": "/todo-app/static/js/2.809488a3.chunk.js"
+  },
+  {
+    "revision": "c27e167d6e2cc1d9eddf7fde2be56a6c",
+    "url": "/todo-app/static/js/2.809488a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "495715a57c09400b39d1",
+    "url": "/todo-app/static/js/main.9a0b17a4.chunk.js"
+  },
+  {
+    "revision": "1800eb705f3eed3b1341",
+    "url": "/todo-app/static/js/runtime-main.06ca33c3.js"
+  }
+]);
